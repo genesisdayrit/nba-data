dc10fd07acf4dde8cac7a661b8b7a964d9023b41
genesisdayrit <genesisdayrit@gmail.com>
2023-06-01 09:59:31 -0500

    script with delete catalog

---
+++ get_schema.py
@@ -0,0 +1,13 @@
+import boto3
+
+def get_table_schema(database_name, table_name):
+    glue = boto3.client('glue')
+
+    response = glue.get_table(DatabaseName=database_name, Name=table_name)
+
+    columns = response['Table']['StorageDescriptor']['Columns']
+
+    return [(col['Name'], col['Type']) for col in columns]
+
+# Test the function
+print(get_table_schema('your_database_name', 'your_table_name'))
